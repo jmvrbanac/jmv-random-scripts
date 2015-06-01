1f99f99dbc81934b676be48abd181bde05d0a1d2
John Vrbanac <john.vrbanac@linux.com>
2015-06-01 16:02:46 -0500

    Switching to pinknoise

--- enterprise.sh
+++ enterprise.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-play -c2 -n synth whitenoise band -n 100 24 band -n 300 100 gain +4  synth pinknoise lowpass -1 100 lowpass -1 100  lowpass -1 100 gain +2
+play -c2 -n synth pinknoise band -n 100 24 band -n 300 100 gain +4  synth pinknoise lowpass -1 100 lowpass -1 100  lowpass -1 100 gain +2
