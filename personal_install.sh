bed81559cdc9dd1bdec536e0deb98775020cb611
John Vrbanac <john.vrbanac@linux.com>
2016-07-04 12:22:13 -0500

    Updating personal install script for Debian testing

--- personal_install.sh
+++ personal_install.sh
@@ -1,56 +1,66 @@
 #!/bin/bash
 
-# Add Sources
-sudo apt-add-repository ppa:yubico/stable
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-
-# sudo apt-get update
+# Editors
 sudo apt-get install -y vim vim-gnome
 
 # Install Git things
 sudo apt-get install -y git git-cola gitg
 
-# Utilities
-sudo apt-get install -y gdebi gparted pavucontrol alacarte silversearcher-ag
-sudo apt-get install -y curl
+# General Utils
+sudo apt-get install -y gdebi gparted pavucontrol alacarte silversearcher-ag wget curl keepassx apt-transport-https ca-certificates
 
 # U2F support
 sudo wget https://raw.githubusercontent.com/Yubico/libu2f-host/master/70-u2f.rules -O /etc/udev/rules.d/70-u2f.rules
 
-# Setting up plugin hookups for vim
+# Set Key Repeats
+echo "xset r rate 200 60" >> ~/.xinitrc
+
+# Vim plugin hookups
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 mkdir ~/.vim-tmp
 
-ln -s ~/Repositories/github/dotfiles/.vimrc ~/.vimrc
-ln -s ~/Repositories/github/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s ~/Repositories/github/dotfiles/.gitconfig ~/.gitconfig
+# Internet Apps
+sudo apt-get install -y hexchat pithos
 
-# Internet Things
-sudo apt-get install -y network-manager-vpnc mumble hexchat pidgin pidgin-otr
+# Installing Docker
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo sh -c "echo 'deb https://apt.dockerproject.org/repo debian-stretch main' > /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
+sudo apt-get install -y docker-engine
 
-# Development
-sudo apt-get install -y lxc-docker
-sudo apt-get install -y python-pip gcc libffi-dev python-dev libssl-dev libsqlite3-dev libxml2-dev libxslt1-dev make build-essential zlib1g-dev libbz2-dev libreadline-dev
-sudo apt-get install -y ruby
-sudo pip install virtualenvwrapper -U
+# Python Dev
+sudo apt-get install -y build-essential python-dev libffi-dev libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev
 
-# Corebird
-sudo apt-get install -y sqlite3 libglib2.0-dev libgtk-3-dev librest-dev valac-0.26 libgee-0.8-dev libjson-glib-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev intltool
-
-# Powersaving
-sudo apt-get install -y laptop-mode-tools
-sudo su -c 'echo "options snd_hda_intel power_save=1" > /etc/modprobe.d/audio_powersave.conf'
-sudo su -c 'echo 5 > /proc/sys/vm/laptop_mode'
-sudo su -c 'echo "options usbcore autosuspend=1" > /etc/modprobe.d/usbcore'
-# sudo su -c 'echo "blacklist uhci_hcd" >> /etc/modprobe.d/blacklist'
-
-# - PyENV
+# - Installing PyEnv
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+# - Install Python Versions
+pyenv install 2.7.12
+pyenv install 3.4.5
+pyenv install 3.5.2
+pyenv install pypy-5.3.1
+pyenv global 2.7.12 3.5.2 3.4.5 pypy-5.3.1
+
+# - Install virtualenvwrapper
+git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
 echo 'pyenv virtualenvwrapper' >> ~/.bashrc
 source ~/.bashrc
+
+# Setup local bin
+echo 'export PATH="~/.local/bin/:$PATH"' >> ~/.bashrc
+
+# Setup folders
+mkdir -p ~/Repositories/github
+mkdir -p ~/Repositories/gitlab
+
+# Setup dotfiles
+git clone git@github.com:jmvrbanac/dotfiles.git ~/Repositories/github/dotfiles
+ln -s ~/Repositories/github/dotfiles/.vimrc ~/.vimrc
+ln -s ~/Repositories/github/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/Repositories/github/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/Repositories/github/dotfiles/.fonts.conf ~/.fonts.config
